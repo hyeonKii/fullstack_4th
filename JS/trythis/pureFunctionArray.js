d39c7eb618559d05778f773941d1431229ce4dfc
hyeonKii <khk5676@gmail.com>
2023-07-19 00:19:11 +0900

    0718

--- JS/trythis/pureFunctionArray.js
+++ JS/trythis/pureFunctionArray.js
@@ -1,31 +1,30 @@
 const arr = [1,2,3,4];
 
-// const push = (arr, ...appendValues) => [...arr, ...appendValues];
-// const pop = (arr, cnt = 1) => {
-//     let result = [];
-//     for (let i = 0; i < cnt; i += 1) {
-//         result = [...result, arr[arr.length - (i + 1)]];
-//     }
-//     return result?.length === 1 ? result[0] : result;
-// }
+const push = (arr, ...appendValues) => [...arr, ...appendValues];
+const pop = (arr, cnt = 1) => {
+    let result = [];
+    for (let i = 0; i < cnt; i += 1) {
+        result = [...result, arr[arr.length - (i + 1)]];
+    }
+    return result?.length === 1 ? result[0] : result;
+}
 
-// const unshift = (arr, ...appendValues) => [...appendValues, ...arr];
+const unshift = (arr, ...appendValues) => [...appendValues, ...arr];
 
 const shift = (arr, cnt = 1) => {
     let result = [];
     for (let i = cnt; i < arr.length; i += 1) {
-        result =  [...result, arr[arr.length - i]];
+        result = [...result, arr[i]];
     }
-    return result?.length === 1 ? [] : result;
+    return result;
 }
 
-
-
-// console.log(push(arr,5,6));
-// console.log(pop(arr));
-// console.log(unshift(arr,0));
-// console.log(unshift(arr, 7, 8));
+console.log(push(arr,5,6));
+console.log(pop(arr));
+console.log(pop(arr, 2));
+console.log(unshift(arr,0));
+console.log(unshift(arr, 7, 8));
 console.log(shift(arr));
-// console.log(shift(arr,2));
+console.log(shift(arr,2));
 
-// console.log(arr);
+console.log(arr);
